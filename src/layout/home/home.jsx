514199602e872cb13e9ee362a581207d9b72e83b
roman.maksimov001 <roman.maksimov001@gmail.com>
2021-04-07 15:39:36 +0300

    edited

--- src/layout/home/home.jsx
+++ src/layout/home/home.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import { SOCIAL_CONFIG, DEPENDENCIES } from './constants';
 import './home.css';
 
 
@@ -29,26 +28,7 @@ const Home = () => (
         переписать под свои задачи, либо расширить его
             функциональность
     </p>
-    <h3><span>Dependencies:</span></h3>
-    <ul className="dependencies">
-      {DEPENDENCIES.map(({ id, name, link }) => (
-        <li className="dependencyItem" key={id}>
-          <a className="dependencyLink" href={link} rel="noopener noreferrer" target="_blank">{name}</a>
-        </li>
-      ))}
-    </ul>
-    <h3><span>Author in social networks:</span></h3>
-    <ul className="socialList">
-      {SOCIAL_CONFIG.map(({
-        id, socialLink, imageLink, imgHeight,
-      }) => (
-        <li key={id} className="socialItem">
-          <a className="socialLink" href={socialLink} rel="noopener noreferrer" target="_blank">
-            <img src={imageLink} height={imgHeight} alt={id} />
-          </a>
-        </li>
-      ))}
-    </ul>
+
   </Fragment>
 );
 
