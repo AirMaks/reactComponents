0cd8ccb33e544ad2f634d19f6036ed1e18ef3fff
roman.maksimov001 <roman.maksimov001@gmail.com>
2021-04-07 15:37:53 +0300

    lists components

---
+++ src/components/list-group/ListGroup.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './ListGroup.css';
+
+const ListGroup = ({
+  children, className, horizontal, tag: Tag, ...attrs
+}) => {
+  const classes = classNames(
+    'list-group',
+    className,
+    { horizontal },
+  );
+
+  return (
+    <Tag className={classes} {...attrs}>
+      {children}
+    </Tag>
+  );
+};
+
+ListGroup.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  horizontal: PropTypes.bool,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+ListGroup.defaultProps = {
+  children: null,
+  className: '',
+  horizontal: false,
+  tag: 'ul',
+};
+
+export default ListGroup;
